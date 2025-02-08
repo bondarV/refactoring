63cf725e6e0c4487e06d447ac5053ff3e7d471ed
Bondar Vitaliy <ipz234_bvo@student.ztu.edu.ua>
2025-02-08 20:16:28 +0200

    refactor: Get rid of duplicate code

--- task07/task.py
+++ task07/task.py
@@ -1,21 +1,26 @@
-class DecimalToRoman:
-    def __init__(self,num):
-        self.roman_numeral = ''
-        self.num = num
+class RomanNumerals:
+    value_map_list = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'),
+        (1, 'I')
+    ]
+    value_map_dict = {symbol: value for value, symbol in value_map_list}
 
-        self.value_map = [
-            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
-            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
-            (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'),
-            (1, 'I')
-        ]
+
+class DecimalToRoman:
+    def __init__(self):
+        self.roman_numeral = ''
+        self.num = 0
 
     def convert(self, num):
+        self.num = num
+        self.roman_numeral = ''
         self.implementation_roman()
         return self.roman_numeral
 
     def implementation_roman(self):
-        for value, symbol in self.value_map:
+        for value, symbol in RomanNumerals.value_map_list:
             self.add_symbols(value, symbol)
 
     def add_symbols(self, value, symbol):
@@ -35,16 +40,12 @@ class DecimalToRoman:
 
 class RomanToDecimal:
     def __init__(self):
-        self.value_map = {
-            'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,
-            'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
-            'X': 10, 'IX': 9, 'V': 5, 'IV': 4,
-            'I': 1
-        }
         self.decimal = 0
         self.i = 0
 
     def convert(self, roman_number):
+        self.decimal = 0
+        self.i = 0
         while self.check_length(roman_number):
             if self.check_on_pair(roman_number):
                 self.found_combo_of_literals(roman_number)
@@ -56,20 +57,20 @@ class RomanToDecimal:
         return self.i < len(roman_number)
 
     def check_on_pair(self, roman_number):
-        return self.check_length(roman_number) and roman_number[self.i:self.i + 2] in self.value_map
+        return self.i + 1 < len(roman_number) and roman_number[self.i:self.i + 2] in RomanNumerals.value_map_dict
 
     def found_solo_literal(self, roman_number):
-        self.decimal += self.value_map[roman_number[self.i]]
+        self.decimal += RomanNumerals.value_map_dict[roman_number[self.i]]
         self.i += 1
 
     def found_combo_of_literals(self, roman_number):
-        self.decimal += self.value_map[roman_number[self.i:self.i + 2]]
+        self.decimal += RomanNumerals.value_map_dict[roman_number[self.i:self.i + 2]]
         self.i += 2
 
 
 if __name__ == '__main__':
     decimal_to_roman_converter = DecimalToRoman()
-    print(decimal_to_roman_converter.convert(1987))  # MCMLXXXVII
+    print(decimal_to_roman_converter.convert(1987))
 
     roman_to_decimal_converter = RomanToDecimal()
-    print(roman_to_decimal_converter.convert('MCMLXXXVII'))  # 1987
+    print(roman_to_decimal_converter.convert('MCMLXXXVII'))
